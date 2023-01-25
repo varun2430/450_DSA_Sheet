0ff69741260481246f1ba648064b37109627d2f8
Varun Kadkade <varunkadkade2442@gmail.com>
2023-01-25 23:21:07 +0530

    array-1

--- array/minimum_no_of_jumps_to_reach_end_of_an_array.cpp
+++ array/minimum_no_of_jumps_to_reach_end_of_an_array.cpp
@@ -1,7 +1,32 @@
 #include<iostream>
 using namespace std;
 
+int getMinJumps(int arr[], int n)
+{
+    int jumps=0, low=0, high=0;
+    while(high < n-1)
+    {
+        int farthest = 0;
+        for(int i=low ; i <= high ; i++)
+        {
+            farthest = max(farthest, i + arr[i]);
+        }
+
+        low = high+1;
+        high = farthest;
+
+        jumps++;
+    }
+
+    return jumps;
+}
+
 int main()
 {
+    int arr[] = { 1, 3, 5, 8, 9, 2, 6, 7, 6, 8, 9 };
+    const int n = sizeof(arr) / sizeof(int);
+
+    cout << getMinJumps(arr, n) << endl;
+
     return 0;
 }
