4452299c155b8fadb454359efa04c6e4caafacee
Varun Kadkade <varunkadkade2442@gmail.com>
2023-01-26 22:23:50 +0530

    array-incomplete

---
+++ array/find_duplicate_in_an_array_of_N+1_integers.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+// simple approach would be to use hashmap to slove in linear time & space
+
+int findDuplicate(int arr[], int n)
+{
+    for(int i=0 ; i < n ; i++)
+    {
+
+    }
+}
