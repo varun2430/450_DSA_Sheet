00b8b5bd70730857f68ff913a3e56a0ec9dfc4b9
Varun Kadkade <varunkadkade2442@gmail.com>
2023-02-07 21:47:59 +0530

    array-1

---
+++ array/best_time_to_buy_and_sell_stock.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int maxProfit(vector<int>& prices)
+{
+    const int n = prices.size();
+    
+    int buy=prices[0], max_profit=0;
+    for(int i=1 ; i < n ; i++)
+    {
+        if(buy > prices[i])
+        {
+            buy = prices[i];
+        }
+
+        max_profit = max(max_profit, prices[i]-buy);
+    }
+
+    return max_profit;
+}
+
+int main()
+{
+    vector<int> prices = {7,1,5,3,6,4};
+
+    cout << maxProfit(prices) << endl;
+
+    return 0;
+}
