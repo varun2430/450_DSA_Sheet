fd1aa4cc165c04abca392018c0460f59b6ac7cb8
Varun Kadkade <varunkadkade2442@gmail.com>
2023-04-07 22:44:34 +0530

    binary_tree-1

---
+++ binary_trees/check_if_all_leaf_nodes_are_at_same_level_or_not.cpp
@@ -0,0 +1,49 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node* left;
+    Node* right;
+};
+
+bool check(Node *root)
+{
+    queue<Node *> q;
+    q.push(root);
+
+    bool leaf_found = false;
+
+    while (!q.empty())
+    {
+        int q_size = q.size();
+        for (int i = 0; i < q_size; i++)
+        {
+            Node *curr = q.front();
+            q.pop();
+
+            if (curr->left != nullptr)
+            {
+                q.push(curr->left);
+            }
+
+            if (curr->right != nullptr)
+            {
+                q.push(curr->right);
+            }
+
+            if ((curr->left == nullptr) && (curr->right == nullptr))
+            {
+                leaf_found = true;
+            }
+        }
+
+        if ((!q.empty()) && (leaf_found == true))
+        {
+            return false;
+        }
+    }
+
+    return true;
+}
