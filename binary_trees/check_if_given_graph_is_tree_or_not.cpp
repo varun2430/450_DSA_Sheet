674e59b93db63b9e0876d697b7673b984583afff
Varun Kadkade <varunkadkade2442@gmail.com>
2023-04-20 21:15:41 +0530

    binary_tree-incomplete

---
+++ binary_trees/check_if_given_graph_is_tree_or_not.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// An undirected graph is a tree if it has the following properties:
+// There is no cycle. 
+// The graph is connected.
+
+int main()
+{
+    return 0;
+}
