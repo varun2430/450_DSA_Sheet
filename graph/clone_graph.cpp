16776c48a120d1bf8f77e5386caedc3c95769eee
Varun Kadkade <varunkadkade2442@gmail.com>
2023-06-17 18:07:39 +0530

    graph-1

---
+++ graph/clone_graph.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int val;
+    vector<Node *> neighbors;
+    Node()
+    {
+        val = 0;
+        neighbors = vector<Node *>();
+    }
+    Node(int _val)
+    {
+        val = _val;
+        neighbors = vector<Node *>();
+    }
+    Node(int _val, vector<Node *> _neighbors)
+    {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+
+unordered_map<Node *, Node *> copies;
+
+Node *cloneGraph(Node *node)
+{
+    if (!node)
+    {
+        return NULL;
+    }
+
+    if (copies.find(node) == copies.end())
+    {
+        copies[node] = new Node(node->val, {});
+        for (Node *neighbor : node->neighbors)
+        {
+            copies[node]->neighbors.push_back(cloneGraph(neighbor));
+        }
+    }
+
+    return copies[node];
+}
